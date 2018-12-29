e399e5c14c2567e9f4e5dc21e9634ab91d2b3dfb
gexiaoshan <gexiaoshan198888>
2018-12-29 11:30:34 +0800

    3.0

--- config-client/src/main/java/com/example/configclient/TestController.java
+++ config-client/src/main/java/com/example/configclient/TestController.java
@@ -1,21 +1,22 @@
 package com.example.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+/**
+ * Created by gexiaoshan on 2018/12/28.
+ */
+@RestController
 @RefreshScope
-public class ConfigClientApplication {
+public class TestController {
 
+    @Value("${test.test}")
+    private String test;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+    @RequestMapping("/test")
+    public String getTest(){
+        return test;
     }
-
-
 }
-
