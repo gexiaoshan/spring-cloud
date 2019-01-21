87a491c0f506563479fc5eb156ec5d094cde6b9a
gexiaoshan <gexiaoshan198888>
2019-01-21 10:42:25 +0800

    zuul

--- eureka-discovery/src/main/java/com/example/eurekadiscovery/TestController.java
+++ eureka-discovery/src/main/java/com/example/eurekadiscovery/TestController.java
@@ -3,6 +3,8 @@ package com.example.eurekadiscovery;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by gexiaoshan on 2019/1/15.
  */
@@ -10,7 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping("/test")
-    public String getTest(){
-        return "eureka-discovery";
+    public String getTest(HttpServletRequest request)
+    {
+        System.out.println("test>>>>>>");
+        try{
+            Thread.sleep(1000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return  "eureka-discovery：" + request.getServerPort();
+    }
+
+    @RequestMapping("/test2")
+    public String getTes2t(HttpServletRequest request)
+    {
+        System.out.println("test2>>>>>>");
+        try{
+            Thread.sleep(2000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return  "eureka-discovery：" + request.getServerPort();
+    }
+
+    @RequestMapping("/test3")
+    public String getTes3t(HttpServletRequest request)
+    {
+        System.out.println("test3>>>>>>");
+        try{
+            Thread.sleep(4100);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return  "eureka-discovery：" + request.getServerPort();
     }
 }
